2c88987a0806d19b471561552c9c9a1dd9e7a4db
Koschei <nitianzero@gmail.com>
2025-02-14 23:18:26 +0800

    feat: implement basic skiplist and project structure

---
+++ src/skiplist.cpp
@@ -0,0 +1,178 @@
+//
+// Created by Koschei on 2025/2/12.
+//
+
+#include "skiplist.h"
+
+#include <iomanip>
+#include <sstream>
+#include <unordered_set>
+
+SkipList::SkipList(int max_level, float prob)
+    : size_bytes_(0),
+      max_level_(max_level),
+      current_level_(1),
+      probability_(prob),
+      gen_(rd_()),
+      dis_(0.0, 1.0) {
+  header_ = std::make_shared<SkipListNode>("", "", max_level_);
+}
+
+int SkipList::random_level() {
+  int level = 1;
+  // 通过"抛硬币"的方式随机生成层数：
+  // - 每次有50%的概率增加一层
+  // - 确保层数分布为：第1层100%，第2层50%，第3层25%，以此类推
+  // - 层数范围限制在[1, max_level]之间，避免浪费内存
+  while (dis_(gen_) < probability_ && level < max_level_) {
+    ++level;
+  }
+  return level;
+}
+
+void SkipList::insert(std::string key, std::string value) {
+  // 保存搜索过程中经过的节点
+  std::vector<std::shared_ptr<SkipListNode> > update(max_level_, nullptr);
+  auto current = header_;  // 不能使用引用，引用就把 header 改了
+  for (int level = current_level_ - 1; level >= 0; --level) {
+    while (current->forward_[level] && current->forward_[level]->key_ < key) {
+      current = current->forward_[level];
+    }
+    update[level] = current;
+  }
+  current = current->forward_[0];
+  if (current && current->key_ == key) {
+    current->value_ = std::move(value);
+  } else {
+    // 随机生成一个层级 level，插入 0 ~ level 层
+    int new_level = random_level();
+    if (new_level > current_level_) {
+      for (int level = current_level_; level < new_level; ++level) {
+        update[level] = header_;
+      }
+      current_level_ = new_level;
+    }
+    size_bytes_ += key.size() + value.size();
+    auto new_node = std::make_shared<SkipListNode>(
+        std::move(key), std::move(value), max_level_);
+    for (int level = 0; level < new_level; ++level) {
+      new_node->forward_[level] = update[level]->forward_[level];
+      update[level]->forward_[level] = new_node;
+    }
+  }
+}
+
+void SkipList::erase(const std::string& key) {
+  // 保存搜索过程中经过的节点
+  std::vector<std::shared_ptr<SkipListNode> > update(max_level_, nullptr);
+  auto current = header_;
+  for (int level = current_level_ - 1; level >= 0; --level) {
+    while (current->forward_[level] && current->forward_[level]->key_ < key) {
+      current = current->forward_[level];
+    }
+    update[level] = current;  // 都是 < key 或 nullptr
+  }
+  // auto &prev = current->forward_[0];
+  if (current->forward_[0] && current->forward_[0]->key_ == key) {
+    current = current->forward_[0];  // 下一个节点可能大于等于
+                                     // key，等于的话就是我要找的了
+    size_bytes_ -= current->key_.size() + current->value_.size();
+    for (int level = 0; level < current_level_; ++level) {
+      // update[level]->forward_[level] =
+      // update[level]->forward_[level]->forward_[level];
+      // 必须是需要删除的目标节点，否则会误删无关节点
+      if (update[level]->forward_[level]->key_ == key) {
+        update[level]->forward_[level] = current->forward_[level];
+      }
+    }
+    // 删除节点可能导致层级降低
+    while (current_level_ > 1 &&
+           header_->forward_[current_level_ - 1] == nullptr) {
+      --current_level_;
+    }
+  }
+}
+
+std::optional<std::string> SkipList::get(const std::string& key) const {
+  auto current = header_;
+  for (int level = current_level_ - 1; level >= 0; --level) {
+    while (current->forward_[level] && current->forward_[level]->key_ < key) {
+      current = current->forward_[level];
+    }
+  }
+  current = current->forward_[0];
+  if (current && current->key_ == key) {
+    return current->value_;
+  }
+  return {};
+}
+
+bool SkipList::contains(const std::string& key) const {
+  return get(key).has_value();
+}
+
+void SkipList::print() const {
+  // 获取底层所有节点并计算最大键长
+  std::vector<std::shared_ptr<SkipListNode> > nodes;
+  auto current = header_->forward_[0];
+  size_t max_key_length = 0;
+  while (current != nullptr) {
+    nodes.emplace_back(current);
+    if (current->key_.length() > max_key_length) {
+      max_key_length = current->key_.length();
+    }
+    current = current->forward_[0];
+  }
+
+  if (nodes.empty()) {
+    std::cout << "Skip list is empty." << std::endl;
+    return;
+  }
+
+  const int node_width = max_key_length + 4;  // 键宽 + 箭头宽度
+
+  for (int level = current_level_ - 1; level >= 0; --level) {
+    std::cout << "Level" << std::right << std::setw(2) << level << ": ";
+
+    // 收集当前层存在的节点键
+    std::unordered_set<std::string> level_keys;
+    auto node = header_->forward_[level];
+    while (node != nullptr) {
+      level_keys.emplace(node->key_);
+      node = node->forward_[level];
+    }
+
+    // 构建当前层的显示字符串
+    std::string level_str;
+    int last_pos = -1;  // 上一个存在节点的位置
+    int loss = 0;
+    for (int j = 0; j < nodes.size(); ++j) {
+      const auto& key = nodes[j]->key_;
+      // 该层存在这个 key 就打印出来
+      if (level_keys.count(key)) {
+        if (last_pos == -1) {
+          // 填充起始空格
+          if (j > 0) {
+            level_str += std::string(j * node_width - loss, ' ');
+          }
+          level_str += key;
+        } else {
+          // 计算间隔并填充箭头
+          int gap = j - last_pos - 1;
+          if (gap > 0) {
+            level_str += " ";
+            level_str += std::string(gap * node_width - loss, '-');
+          }
+          level_str += gap > 0 ? "-> " : " -> ";
+          level_str += key;
+        }
+        loss = 0;
+        last_pos = j;
+      } else {
+        loss += max_key_length - key.size();
+      }
+    }
+    // 打印这层
+    std::cout << level_str << std::endl;
+  }
+}
---
+++ src/skiplist.h
@@ -0,0 +1,60 @@
+//
+// Created by Koschei on 2025/2/12.
+//
+
+#ifndef SKIPLIST_H
+#define SKIPLIST_H
+
+#include <__random/random_device.h>
+
+#include <iostream>
+#include <memory>
+#include <random>
+#include <utility>
+#include <vector>
+
+struct SkipListNode {
+  std::string key_;                                      // 节点存储的键
+  std::string value_;                                    // 节点存储的值
+  std::vector<std::shared_ptr<SkipListNode> > forward_;  // 多层前向指针
+
+  SkipListNode(std::string key, std::string value, int level)
+      : key_(std::move(key)),
+        value_(std::move(value)),
+        forward_(level, nullptr) {}
+};
+
+class SkipList {
+ public:
+  explicit SkipList(int max_level = 16, float prob = 0.5);
+
+  SkipList(const SkipList&) = delete;
+
+  SkipList& operator=(const SkipList&) = delete;
+
+  void insert(std::string key, std::string value);
+
+  void erase(const std::string& key);
+
+  std::optional<std::string> get(const std::string& key) const;
+
+  bool contains(const std::string& key) const;
+
+  size_t get_size() const { return size_bytes_; }
+
+  void print() const;
+
+ private:
+  size_t size_bytes_;
+  int max_level_;
+  int current_level_;
+  float probability_;
+  std::shared_ptr<SkipListNode> header_;
+  std::random_device rd_;
+  std::mt19937 gen_;
+  std::uniform_real_distribution<> dis_;
+
+  int random_level();
+};
+
+#endif  // SKIPLIST_H
